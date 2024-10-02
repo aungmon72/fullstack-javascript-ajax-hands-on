71f5c99e2117528069154f1656bb1818e4f2bfc1
aungmon72 <aungmon72@yahoo.com>
2024-10-02 07:10:53 +0000

    Commit

--- 1-Hello-World/index.js
+++ 1-Hello-World/index.js
@@ -1,1 +1,11 @@
 
+document.addEventListener("DOMContentLoaded",async function() {
+    let url = "https://4geeksacademy.github.io/exercise-assets/txt/hello.txt"
+    // let data = await 
+    let content = document.querySelector("#content")
+    let response = await axios.get(url)
+    let responseData = response.data
+    console.log(responseData)
+
+    content.innerHTML=responseData
+})
